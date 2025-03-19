57a75daec8aa531ef444127abe030d0000705656
joão Vitor <joaovitormachadodev99@gmail.com>
2025-03-19 18:28:18 -0300

    att

--- contatos.py
+++ contatos.py
@@ -49,7 +49,7 @@ class Ui_Form(object):
     def filtrar_contatos(self):
         texto_busca = self.line_buscar_cntt.text().lower()
     
-    # Itera sobre todos os contatos e seus widgets associados
+   
         for i, label in enumerate(self.labels_contatos):
         
             visivel = texto_busca in label.text().lower()
